9a233de33fd5636c1384dff4e0adf64cb3743cb5
sheylah98 <148611355+sheylah98@users.noreply.github.com>
2023-10-21 12:44:18 +0300

    vscode

--- mabrook fytness/MABROOK VS/mabvs/android/app/src/main/kotlin/com/example/mabvs/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mabvs
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mabrook fytness/MABROOK VS/mabvs/mbv/android/app/src/main/kotlin/com/example/mbv/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mbv
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
